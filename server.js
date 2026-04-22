ea9725c1e2a2747db417074b2d8ad79cf5f0d212
Tom Quinn <mathtutor.tomquinn@gmail.com>
2026-04-21 23:51:37 -0400

    Fix port for Render

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.post('/verify-payment', async (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
