627493232da83de079678009bb7fa7f9b6b5e156
Tom Quinn <mathtutor.tomquinn@gmail.com>
2026-04-30 14:38:15 -0400

    force rebuild real change

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const app = express();
+app.use(express.json());
 
 app.use(cors({
   origin: [
@@ -13,9 +15,6 @@ app.use(cors({
 }));
 
 app.options("*", cors());
-
-const app = express();
-app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
