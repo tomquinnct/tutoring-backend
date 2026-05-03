8a0b8721f1f8cdbad9fa6e083b95644322aba980
Tom Quinn <mathtutor.tomquinn@gmail.com>
2026-05-03 18:20:48 -0400

    force rebuild real change

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ app.use(session({
   }
 }));
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 // =======================
 // ROUTES
 // =======================
