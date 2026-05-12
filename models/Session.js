5414e78c39fc92ae34c1524801a3c821e2e924bb
Tom Quinn <mathtutor.tomquinn@gmail.com>
2026-05-12 14:26:15 -0400

    fix accessToken scope crash

--- models/Session.js
+++ models/Session.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const sessionSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  sessionsRemaining: { type: Number, default: 0 }
+  userId: {
+    type: String,
+    required: true
+  },
+
+  sessions: {
+    type: Number,
+    default: 0
+  }
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
